e56e0ed1a9f1f97c1b86009509f63562e7e56b7a
Tim Medley <tim@medley.us>
2018-03-07 18:37:44 -0500

    add elvui release version check
    messy but it works using beautiful soup

--- elvUIClientapp.py
+++ elvUIClientapp.py
@@ -1,19 +1,21 @@
-
 #! /usr/bin/env python3
 
 
 #    ElvUIMacClient
 #    macOS client to update ElvUI Addon for World of Warcraft
 #    Tim Medley bloaf on Turalyon bloaf@medley.us
-#    v0.15
+#    v0.17
 #    3/7/2018
-#    next steps:
+#    // TODO:
 
 
 import platform
 import subprocess
 import os
 from os import system
+import requests
+from bs4 import BeautifulSoup
+import json
 
 
 class ElvUIClientApp():
@@ -58,7 +60,7 @@ class ElvUIClientApp():
             with open(elvLocation, 'rt') as elvFile:
                 for line in elvFile:
                     lines.append(line)
-            elvUIVersion = lines[2][12:18]
+            elvUIVersion = lines[2][12:17]
 
             return (elvUIInstalled, elvUIVersion)
         else:
@@ -66,23 +68,63 @@ class ElvUIClientApp():
 
             return elvUIInstalled
 
-    def getReleaseElvUIVersion():
+    def getReleaseElvUIVersion(self):
         # get current elvUI release version
-        pass
+        # initial method is to use beautiful soup and scrape the current
+        # version from the website https://www.tukui.org/download.php?ui=elvui
+        # this wont scale, but sorta works
+        #
+        #// TODO: clean this up and maybe use the gitlabs or git modules
 
-    def upgradeElvUI():
+        elvUIReleaseURL = 'https://www.tukui.org/download.php?ui=elvui#version'
+        elvUIWebPage = requests.get(elvUIReleaseURL)
+        soup = BeautifulSoup(elvUIWebPage.text, 'html.parser')
+
+        versionSection = soup.find("div", class_="tab-pane fade", id="version")
+        elvUIReleaseVersionRaw = versionSection.text.strip()
+        elvUIReleaseVersion = elvUIReleaseVersionRaw[32:37]
+        return elvUIReleaseVersion
+
+    def upgradeElvUI(self):
+        # Get the latest elvUI addon from the tukui.org website
+        # remove the existing elvUI and elvUI_Config folders
+        # the nice thing to do is toss them in the trash
+        # worst case we can just delete them
+        #
         # when finished play a cute sound
         # system('afplay ~/ElvUIClientapp/client/sounds/jobsdone.mp3')
+        pass
+
+    def tukUIAPI(self):
+        # access the tukUI.org web API
+        # for user and rank recognition
+        #
+        # baseURL='https://www.tukui.org/api.php?'
+
+        # tukUsername=input(
+        #'''\n
+        # We need to log into the https: / www.tukui.org website
+        # Enter your Username:
+        #''')
+
+        #tukPassword=input('Enter your Password: ')
+
         pass
 
 
 main = ElvUIClientApp()
 wowVersion = main.getWoWVersion()
 macVersion = main.getMacOSVersion()
-elvUIVersion = main.getElvUIInstalledVersion()
-
+elvUIInstalledVersion = main.getElvUIInstalledVersion()
+elvUIReleaseVersion = main.getReleaseElvUIVersion()
 
 print('macOS version: ', macVersion)
 print('Application version: ', wowVersion)
-print('Is ElvUI Installed? ', elvUIVersion[0])
-print('ElvUI Installed version: ', elvUIVersion[1])
+print('Is ElvUI Installed? ', elvUIInstalledVersion[0])
+print('ElvUI Installed version: ', elvUIInstalledVersion[1])
+
+if elvUIInstalledVersion[1] == elvUIReleaseVersion:
+    print('ElvUI is up to update',
+          'ElvUI Release version: ', elvUIReleaseVersion)
+else:
+    print('ElvUI is out of date, you should upgrade it to', elvUIReleaseVersion)
